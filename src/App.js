28ffc1cd5e6222391dee295a208ae60ff9cbddcf
Tim Bauer <timbauer@ymail.com>
2021-10-21 12:56:13 -0700

    part 1 complete

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import { useState } from 'react';
+import { useState, useEffect } from "react";
 
 function App() {
-  const [resourceType, setResourceType] = useState('posts')
+    const [resourceType, setResourceType] = useState("posts");
+    const [items, setItems] = useState([])
 
-  return (
-    <>
-      <div>
-        <button onClick={() => setResourceType('posts')}>Posts</button>
-        <button onClick={() => setResourceType('users')}>Users</button>
-        <button onClick={() => setResourceType('comments')}>Comments</button>
-      </div>
-      <h1>{resourceType}</h1>
-    </>
-  )
+    useEffect(() => {
+        fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+            .then((response) => response.json())
+            .then((json) => setItems(json));
+    }, [resourceType]);
+
+    return (
+        <>
+            <div>
+                <button onClick={() => setResourceType("posts")}>Posts</button>
+                <button onClick={() => setResourceType("users")}>Users</button>
+                <button onClick={() => setResourceType("comments")}>
+                    Comments
+                </button>
+            </div>
+            <h1>{resourceType}</h1>
+            {items.map(item => {return <pre>{JSON.stringify(item)}</pre>})}
+        </>
+    );
 }
 
 export default App;
