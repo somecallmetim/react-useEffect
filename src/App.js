4a95ad30f01afcbc550cace0d73d7d74efdee3f2
Tim Bauer <timbauer@ymail.com>
2021-10-21 13:08:26 -0700

    tutorial complete

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
 import { useState, useEffect } from "react";
 
 function App() {
-    const [resourceType, setResourceType] = useState("posts");
-    const [items, setItems] = useState([])
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+    const handleResize = () => {
+        setWindowWidth(window.innerWidth);
+    };
 
     useEffect(() => {
-        fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
-            .then((response) => response.json())
-            .then((json) => setItems(json));
-    }, [resourceType]);
+        window.addEventListener("resize", handleResize);
 
-    return (
-        <>
-            <div>
-                <button onClick={() => setResourceType("posts")}>Posts</button>
-                <button onClick={() => setResourceType("users")}>Users</button>
-                <button onClick={() => setResourceType("comments")}>
-                    Comments
-                </button>
-            </div>
-            <h1>{resourceType}</h1>
-            {items.map(item => {return <pre>{JSON.stringify(item)}</pre>})}
-        </>
-    );
+        return window.removeEventListener("resize", handleResize);
+    }, []);
+
+    return <div>{windowWidth}</div>;
 }
 
 export default App;
